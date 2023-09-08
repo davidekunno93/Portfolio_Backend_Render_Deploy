f174d2001d23bdb6bc3e9b82b58aee233a76b60e
David Ekunno <matramere@gmail.com>
2023-09-07 22:10:49 -0500

    run changes

--- run.py
+++ run.py
@@ -1,26 +1,30 @@
 from flask import Flask
 from flask_migrate import Migrate
 
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    from app.models import db
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
-from app.models import db
-db.init_app(app)
-migrate = Migrate(app, db)
+    from app.auth.routes import auth
+    app.register_blueprint(auth)
 
-from app.auth.routes import auth
-app.register_blueprint(auth)
+    from app.api.routes import api
+    app.register_blueprint(api)
 
-from app.api.routes import api
-app.register_blueprint(api)
+    import os
 
-import os
+    basedir = os.path.abspath(os.path.dirname(__name__))
 
-basedir = os.path.abspath(os.path.dirname(__name__))
+    class Config():
+        FLASK_APP = os.environ.get('FLASK_APP')
+        FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        SQLALCHEMY_TRACK_NOTIFICATION=False
 
-class Config():
-    FLASK_APP = os.environ.get('FLASK_APP')
-    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_NOTIFICATION=False
+    return app
+
+app = create_app()
